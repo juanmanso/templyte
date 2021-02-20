b7d76878b20a460c8ddc1317cfe3556f0ceb7fb1
Juan Manso <juanmanso@gmail.com>
2021-02-20 13:30:15 -0300

    feat: [cli] consume wikipedia API
    To do so, we used the library requests that performs HTTP requests. Then, we parsed the response and returned to the terminal the Title (formatted in green) and the Extract of the random article

--- src/cli/console.py
+++ src/cli/console.py
@@ -1,11 +1,24 @@
+import textwrap
+
 import click
+import requests
 
 from . import __version__
 
+
+# TODO: detect user's language using `locale`
+API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
 @click.command()
 @click.version_option(version=__version__)
 def main():
     """CLI for the templyte project."""
-    click.echo("Hello, world!")
+    with requests.get(API_URL) as response:
+        response.raise_for_status()
+        data = response.json()
 
+    title = data["title"]
+    extract = data["extract"]
+
+    click.secho(title, fg="green")
+    click.echo(textwrap.fill(extract))

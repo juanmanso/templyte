8a5c55b024e5c0bfd06bfe7b414d78ef1929db1b
Juan Manso <juanmanso@gmail.com>
2021-02-21 04:02:00 -0300

    refactor: [cli] move request's logic to separated file

--- src/cli/console.py
+++ src/cli/console.py
@@ -3,19 +3,14 @@ import textwrap
 import click
 import requests
 
-from . import __version__
+from . import __version__, wikipedia
 
-
-# TODO: detect user's language using `locale`
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
 @click.command()
 @click.version_option(version=__version__)
 def main():
     """CLI for the templyte project."""
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        data = response.json()
+    data = wikipedia.random_page()
 
     title = data["title"]
     extract = data["extract"]
